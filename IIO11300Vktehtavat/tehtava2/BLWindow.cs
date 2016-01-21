dd39618bb3cb0b7837094bb7cf07471d5507f177
H8404 <h8404@student.jamk.fi>
2016-01-21 15:09:51 +0200

    Rivien määrä puuttuu

---
+++ IIO11300Vktehtavat/tehtava2/BLWindow.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tehtava2
+{
+    class BLWindow
+    {
+        public static string DrawRandom()
+        {
+            Random random = new Random();
+
+            int[] randomNumbers = new int[7];
+
+            for (int i = 0; i < randomNumbers.Length; i++)
+            {
+                randomNumbers[i] = random.Next(1, 40);
+            }
+
+
+            return string.Join(" ",randomNumbers);
+        }
+        public static string DrawViking()
+        {
+            Random random = new Random();
+
+            int[] randomNumbers = new int[6];
+
+            for (int i = 0; i < randomNumbers.Length; i++)
+            {
+                randomNumbers[i] = random.Next(1, 49);
+            }
+
+            return string.Join(" ", randomNumbers);
+        }
+        public static string DrawEuro()
+        {
+            Random random = new Random();
+
+            int[] numbers = new int[5];
+            int[] stars = new int[2];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(1, 51);
+            }
+            for (int i = 0; i < stars.Length; i++)
+            {
+                stars[i] = random.Next(1, 9);
+            }
+            return string.Join(" ", numbers) + " " + string.Join(" ",stars);
+        }
+    }
+}
---
+++ IIO11300Vktehtavat/tehtava2/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace tehtava2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void draw_Click(object sender, RoutedEventArgs e)
+        {
+            int boxIndex = comboBox.SelectedIndex;
+            int r = Int32.Parse(rows.Text);
+            if (boxIndex == 0)
+            {
+                numbers.Text = BLWindow.DrawRandom();
+            }
+            if (boxIndex == 1)
+            {
+                numbers.Text = BLWindow.DrawViking();
+            }
+            if (boxIndex == 2) {
+                numbers.Text = BLWindow.DrawEuro();
+            }
+        }
+
+        private void clear_Click(object sender, RoutedEventArgs e)
+        {
+            rows.Text = "1";
+            numbers.Clear();
+            comboBox.SelectedIndex = -1;
+        }
+    }
+}
