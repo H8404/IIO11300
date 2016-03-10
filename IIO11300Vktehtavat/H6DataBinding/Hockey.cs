4a04c50a2884c29a5f1c6c862471d845d906175b
H8404 <H8404@student.jamk.fi>
2016-03-10 10:53:22 +0200

    H7 tehty

--- IIO11300Vktehtavat/H6DataBinding/Hockey.cs
+++ IIO11300Vktehtavat/H6DataBinding/Hockey.cs
@@ -1,13 +1,90 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace H6DataBinding
 {
-    public class HockeyPlayer
+    public static class TestHockeyBench
     {
+        public static List<HockeyPlayer> Get3Players()
+        {
+            List<HockeyPlayer> players = new List<HockeyPlayer>();
+            players.Add(new HockeyPlayer("Teemu Selänne", "8"));
+            players.Add(new HockeyPlayer("Jarkko immonen", "26"));
+            players.Add(new HockeyPlayer("Ville Peltonen", "16"));
+            return players;
+        }
+    }
+    public class HockeyPlayer: INotifyPropertyChanged
+    {
+        #region PROPERTIES
+        private string name;
+        public String Name
+        {
+            set
+            {
+                name = value;
+                Notify("Name");
+            }
+            get
+            {
+                return name;
+            }
+        }
+        private string number;
+        public String Number
+        {
+            set
+            {
+                number = value;
+                Notify("Number");
+            }
+            get
+            {
+                return number;
+            }
+           
+        }
+        public string NameAndNumber
+        {
+            set
+            {
+                Notify("NameAndNumber");
+            }
+            get
+            {
+                return name + "#" + number;
+            }
+        }
+
+
+        #endregion
+        #region CONSTRUCTORS
+        public HockeyPlayer()
+        {
+
+        }
+
+        public HockeyPlayer(string name, string number)
+        {
+            this.name = name;
+            this.number = number;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        void Notify(string propName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+        #endregion
+
+
 
     }
     public class HockeyTeam
@@ -29,10 +106,12 @@ namespace H6DataBinding
             City = city;
         }
         #endregion
+        #region METHODS
         public override string ToString()
         {
             return Name + "@" + City;
         }
+        #endregion
     }
     public class HockeyLeague
     {
---
+++ IIO11300Vktehtavat/H7ADONETConsole/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace H7ADONETConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //1 LUODAAN YHTEYS
+                string connStr = H7ADONETConsole.Properties.Settings.Default.Tietokanta;
+                using (SqlConnection conn = new SqlConnection(connStr))
+                {
+                    conn.Open();
+                    //2 LUODAAN KOMENTO JA SUORITETAAN SE
+                    SqlCommand cmd = new SqlCommand("SELECT asioid, lastname, firstname, date FROM presences WHERE asioid = 'H8404'", conn);
+                    //3 KÄYDÄÄÄN TULOS=READER-OLIO LÄPI
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    Console.WriteLine("Yhteys tietokantaan avattu");
+                    //KÄYDÄÄN rdr LÄPI
+                    if (rdr.HasRows)
+                    {
+                        int lkm = 0;
+                        while (rdr.Read())
+                        {
+                            //TULOS NÄKYVIIN
+                            Console.WriteLine("Läsnäolosi {0} {1} {2} {3}", rdr.GetString(0), rdr.GetString(1), rdr.GetString(2), rdr.GetDateTime(3));
+                            lkm++;
+                        }
+                        Console.WriteLine("Tulostettu {0} läsnäoloa", lkm);
+                    }
+                    //MUISTA MYÖS SULKEA YHTEYS
+                    rdr.Close();
+                    conn.Close();
+                    Console.WriteLine("Tietokanta yhteys suljettu onnistuneesti");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
