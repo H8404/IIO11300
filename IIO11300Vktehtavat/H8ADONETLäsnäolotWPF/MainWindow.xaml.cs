acd589e0937ce5e4c8da230c309c673b175e636e
H8404 <H8404@student.jamk.fi>
2016-03-10 11:09:29 +0200

    H8 luotu

---
+++ IIO11300Vktehtavat/H8ADONETLäsnäolotWPF/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Data; //Sisältää ADO:n perusluokat
+using System.Data.SqlClient; // SQL Server spesifiset luokat
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace H8ADONETLäsnäolotWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            GetData();
+        }
+        private void GetData()
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(H8ADONETLäsnäolotWPF.Properties.Settings.Default.Tietokanta))
+                {
+                    //YHTEYS
+                    //DaTAADAPTER
+                    string sql = "SELECT asioid, lastname, firstname, date FROM presences WHERE asioid = 'H8404'";
+                    SqlDataAdapter da = new SqlDataAdapter(sql, conn);
+                    DataTable dt = new DataTable("Läsnäolot");
+                    da.Fill(dt);
+
+                    //SIDOTAAN datatable ui kontrolliin
+                    myGrid.DataContext = dt;
+                    conn.Close();
+                }
+
+            }
+            catch (Exception ex)
+            {
+               MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
